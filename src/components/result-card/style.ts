9e467f005e45fd6408f48c630c123bc5e91b13db
Julia Mazzoni <juliarmazzoni@gmail.com>
2025-03-18 10:05:55 +1100

    Add animation on ResultCard

--- src/components/result-card/style.ts
+++ src/components/result-card/style.ts
@@ -4,12 +4,13 @@ import styled from "styled-components"
 export const StyledResultCard = styled.div`
   display: grid;
   gap: 15px;
-  background-color: #303030;;
+  background-color: #303030;
   justify-items: center;
   align-content: space-between;
   padding: 20px 0;
   border-radius: 5px;
   color: #fff;
+  transition: transform 0.3s ease-in-out, background-color 0.3s ease-in-out;; /* smooth animation on card */
 
   h1 {
     font-size: 16px;
@@ -21,5 +22,11 @@ export const StyledResultCard = styled.div`
 
   h4 {
     font-size: 18px;
+  }
+
+  &:hover {
+    cursor: pointer;
+    transform: scale(1.05); /* Slightly enlarges the card */
+    background-color: #4f4f4f;
   }
 `
